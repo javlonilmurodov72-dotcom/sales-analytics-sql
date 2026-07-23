d51becc9dd1652d205aad428d4e549f17e189f21
Javlon Ilmurodov <javlonilmurodov72@gmail.com>
2026-07-23 16:31:30 +0500

    Alter data types for date and numeric columns

--- sql/02_data_cleaning.sql
+++ sql/02_data_cleaning.sql
@@ -9,3 +9,11 @@
 ALTER TABLE superstore 
     ALTER COLUMN order_date TYPE DATE USING order_date::DATE,
     ALTER COLUMN ship_date TYPE DATE USING ship_date::DATE;
+
+-- 2. Pul va miqdoriy ustunlarni doimiy raqamlar formatiga o'tkazish
+ALTER TABLE superstore 
+    ALTER COLUMN row_id TYPE INT USING row_id::INT,
+    ALTER COLUMN sales TYPE NUMERIC(15,4) USING sales::NUMERIC(15,4),
+    ALTER COLUMN quantity TYPE INT USING quantity::INT,
+    ALTER COLUMN discount TYPE NUMERIC(15,4) USING discount::NUMERIC(15,4),
+    ALTER COLUMN profit TYPE NUMERIC(15,4) USING profit::NUMERIC(15,4);
